27634eb5e9d5107bb686e48811a68ea9be795647
cataclism <69798041+cataclism@users.noreply.github.com>
2020-10-09 14:30:12 +0200

    Raised Y_CURRENT

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2249,7 +2249,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       725
+    #define Y_CURRENT       800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
